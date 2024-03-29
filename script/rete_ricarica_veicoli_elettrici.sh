265a8152d203c9dee1dc1429e64639f4e42be1cd
aborruso <aborruso@gmail.com>
2024-03-29 16:16:15 +0100

    update

--- script/rete_ricarica_veicoli_elettrici.sh
+++ script/rete_ricarica_veicoli_elettrici.sh
@@ -20,13 +20,15 @@ mkdir -p "$folder"/../data
 
 URL="https://services2.arcgis.com/pROHh69WvVijk4nR/ArcGIS/rest/services/IdR_latest_ready/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token="
 
+# scarica dati e converti in CSV
 ogr2ogr -f CSV -lco GEOMETRY=AS_XY  "$folder"/../data/rete_ricarica_veicoli_elettrici.csv  "$URL" OGRGeoJSON
 
-
+# ordina i dati per ID_univoco_EVSE, così il diff del versioning sarà più lite
 mlrgo -S -I --csv sort -f ID_univoco_EVSE "$folder"/../data/rete_ricarica_veicoli_elettrici.csv
 
-# cleaned_data
+### cleaned_data ###
 
+# copia il file originale in un nuovo file
 cp -f "$folder"/../data/rete_ricarica_veicoli_elettrici.csv "$folder"/../data/rete_ricarica_veicoli_elettrici_cleaned.csv
 
 characters=("Ã¨" "Ã©" "Ã¹" "Ã²" "Ã¬" "Ãª" "Ã")
