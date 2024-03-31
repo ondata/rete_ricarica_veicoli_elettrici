d2c22f3ff9e8c91925b47cdf25c8887013f3a596
aborruso <aborruso@gmail.com>
2024-03-31 09:56:07 +0200

    errore codice sh

--- script/rete_ricarica_veicoli_elettrici_api.sh
+++ script/rete_ricarica_veicoli_elettrici_api.sh
@@ -19,7 +19,7 @@ folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mkdir -p "$folder"/output
 mkdir -p "$folder"/../data/api
 
-# node "$folder"/rete_ricarica_veicoli_elettrici.js
+node "$folder"/rete_ricarica_veicoli_elettrici.js
 
 # if "$folder"/output/rete_ricarica_veicoli_elettrici.jsonl exists, delete it
 if [ -f "$folder"/output/rete_ricarica_veicoli_elettrici.jsonl ]; then
