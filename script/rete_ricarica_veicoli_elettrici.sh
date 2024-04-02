de20b883de642af825cbc5e0840971d6f4893135
aborruso <aborruso@gmail.com>
2024-04-02 07:53:18 +0200

    rimosso scarico dati auto

--- script/rete_ricarica_veicoli_elettrici.sh
+++ script/rete_ricarica_veicoli_elettrici.sh
@@ -81,6 +81,9 @@ mlrgo -S --csv join --ul -j regione_cleaned,comune_cleaned -f "${folder}"/../dat
 
 mv "${folder}"/output/rete_ricarica_veicoli_elettrici_cleaned_istat.csv "${folder}"/../data/rete_ricarica_veicoli_elettrici_cleaned.csv
 
+
+exit 0
+
 ### Dati sulle automobili elettriche ###
 
 URL_auto="https://services2.arcgis.com/pROHh69WvVijk4nR/ArcGIS/rest/services/Province_Italiane_BEV/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&returnEnvelope=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token="
